78e88e220f9c11bf01a3139088c458e706f17c26
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-12 17:35:47 -0400

    feat(ui-components): add FeatureCard component

---
+++ ui-components/FeatureCard.jsx
@@ -0,0 +1,140 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import ReacticonsbiBiCheck from "./ReacticonsbiBiCheck";
+import { Flex, Text } from "@aws-amplify/ui-react";
+export default function FeatureCard(props) {
+  const { FeatureModel, overrides, ...rest } = props;
+  return (
+    <Flex
+      gap="16px"
+      direction="column"
+      width="548px"
+      height="244px"
+      justifyContent="center"
+      alignItems="flex-start"
+      position="relative"
+      boxShadow="0px 2px 6px rgba(0.05098039284348488, 0.10196078568696976, 0.14901961386203766, 0.15000000596046448)"
+      borderRadius="12px"
+      padding="35px 40px 35px 40px"
+      backgroundColor="rgba(226,98,24,1)"
+      {...getOverrideProps(overrides, "FeatureCard")}
+      {...rest}
+    >
+      <Flex
+        gap="16px"
+        direction="column"
+        width="unset"
+        height="unset"
+        justifyContent="center"
+        alignItems="center"
+        grow="1"
+        shrink="1"
+        basis="0"
+        alignSelf="stretch"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Frame 5602")}
+      >
+        <Flex
+          gap="16px"
+          direction="row"
+          width="unset"
+          height="unset"
+          justifyContent="center"
+          alignItems="center"
+          shrink="0"
+          alignSelf="stretch"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Frame 5603")}
+        >
+          <Flex
+            gap="0"
+            direction="row"
+            width="unset"
+            height="unset"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            shrink="0"
+            position="relative"
+            borderRadius="8px"
+            padding="8px 8px 8px 8px"
+            backgroundColor="rgba(241,178,142,1)"
+            {...getOverrideProps(overrides, "Frame 430")}
+          >
+            <ReacticonsbiBiCheck
+              width="24px"
+              height="24px"
+              display="block"
+              gap="unset"
+              alignItems="unset"
+              justifyContent="unset"
+              shrink="0"
+              position="relative"
+              padding="0px 0px 0px 0px"
+              {...getOverrideProps(overrides, "react-icons/bi/BiCheck")}
+            ></ReacticonsbiBiCheck>
+          </Flex>
+          <Text
+            fontFamily="Inter"
+            fontSize="24px"
+            fontWeight="600"
+            color="rgba(13,26,38,1)"
+            lineHeight="30px"
+            textAlign="left"
+            display="block"
+            direction="column"
+            justifyContent="unset"
+            width="unset"
+            height="unset"
+            gap="unset"
+            alignItems="unset"
+            grow="1"
+            shrink="1"
+            basis="0"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children={FeatureModel?.name}
+            {...getOverrideProps(overrides, "Feature A")}
+          ></Text>
+        </Flex>
+        <Text
+          fontFamily="Inter"
+          fontSize="16px"
+          fontWeight="400"
+          color="rgba(48,64,80,1)"
+          lineHeight="24px"
+          textAlign="left"
+          display="block"
+          direction="column"
+          justifyContent="unset"
+          letterSpacing="0.01px"
+          width="unset"
+          height="unset"
+          gap="unset"
+          alignItems="unset"
+          grow="1"
+          shrink="1"
+          basis="0"
+          alignSelf="stretch"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          whiteSpace="pre-wrap"
+          children={FeatureModel?.Description}
+          {...getOverrideProps(
+            overrides,
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."
+          )}
+        ></Text>
+      </Flex>
+    </Flex>
+  );
+}
