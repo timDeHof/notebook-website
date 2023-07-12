e4f5cabeeab3d964f9ef97952e439aadb3f58099
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-12 17:39:42 -0400

    refactor: logo 3 component

---
+++ ui-components/Logo3.d.ts
@@ -0,0 +1,69 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
+import { IconProps, ViewProps } from "@aws-amplify/ui-react";
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type Logo3OverridesProps = {
+    Logo3?: PrimitiveOverrideProps<ViewProps>;
+    NimbusNote39833501?: PrimitiveOverrideProps<IconProps>;
+    NimbusNote39833502?: PrimitiveOverrideProps<IconProps>;
+    NimbusNote39833503?: PrimitiveOverrideProps<IconProps>;
+    NimbusNote39833504?: PrimitiveOverrideProps<IconProps>;
+    NimbusNote39833505?: PrimitiveOverrideProps<IconProps>;
+    NimbusNote39833506?: PrimitiveOverrideProps<IconProps>;
+    NimbusNote39833507?: PrimitiveOverrideProps<IconProps>;
+    NimbusNote39833508?: PrimitiveOverrideProps<IconProps>;
+    NimbusNote39833509?: PrimitiveOverrideProps<IconProps>;
+    NimbusNote39833510?: PrimitiveOverrideProps<IconProps>;
+    "Group 1"?: PrimitiveOverrideProps<ViewProps>;
+    Union39691761?: PrimitiveOverrideProps<IconProps>;
+    Union39691762?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 139691763"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 239691764"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 339691765"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 439691766"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 539691767"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 639691768"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 739691769"?: PrimitiveOverrideProps<IconProps>;
+    Union39691770?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 139691771"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 239691772"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 339691773"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 439691774"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 539691775"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 639691776"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 739691777"?: PrimitiveOverrideProps<IconProps>;
+    Union39691778?: PrimitiveOverrideProps<IconProps>;
+    Union39691779?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 139691780"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 239691781"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 339691782"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 439691783"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 539691784"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 639691785"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 739691786"?: PrimitiveOverrideProps<IconProps>;
+    Union39691787?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 239691788"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 339691789"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 439691790"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 539691791"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 11"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 10"?: PrimitiveOverrideProps<IconProps>;
+    Subtract39691794?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 8"?: PrimitiveOverrideProps<IconProps>;
+    "Ellipse 9"?: PrimitiveOverrideProps<IconProps>;
+    Subtract39691797?: PrimitiveOverrideProps<IconProps>;
+} & EscapeHatchProps;
+export declare type Logo3Props = React.PropsWithChildren<Partial<ViewProps> & {
+    Small?: Boolean;
+} & {
+    small?: "false" | "true";
+} & {
+    overrides?: Logo3OverridesProps | undefined | null;
+}>;
+export default function Logo3(props: Logo3Props): React.ReactElement;
