4d1174e43234b8add10bc2b6d45978a5c9ec7aa1
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-12 17:46:07 -0400

    feat(ui-components): add ReacticonsbiBiBook, ReacticonsbiBiBookAdd, ReacticonsbiBiBookOpen, and ReacticonsbiBiCheck components
    feat(ui-components): add new ReacticonsbiBiCheck component
    feat(ui-components): add new ReacticonsbiBiChevronLeft component
    feat(ui-components): add new ReacticonsbiBiChevronRight component
    feat(ui-components): add new ReacticonsbiBiMenu component
    feat(ui-components): add new ReacticonsbiBiMenuAltLeft component
    
    feat(ui-components): add ReacticonsbiBiMenuAltRight component
    
    This commit adds the ReacticonsbiBiMenuAltRight component to the ui-components directory. The component is generated with Amplify Studio and should not be modified directly. Any changes made to this file will be overwritten when running amplify pull. The component is available as both a TypeScript (.d.ts) and JavaScript (.jsx) file.

---
+++ ui-components/ReacticonsbiBiChevronLeft.jsx
@@ -0,0 +1,36 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Icon } from "@aws-amplify/ui-react";
+export default function ReacticonsbiBiChevronLeft(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <Icon
+      width="24px"
+      height="24px"
+      display="block"
+      gap="unset"
+      alignItems="unset"
+      justifyContent="unset"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      viewBox={{ minX: 0, minY: 0, width: 24, height: 24 }}
+      paths={[
+        {
+          d: "M5.707 0L0 5.707L5.707 11.414L7.121 10L2.828 5.707L7.121 1.414L5.707 0Z",
+          fill: "rgba(0,0,0,1)",
+          fillRule: "nonzero",
+          style: { transform: "translate(31.61%, 26.22%)" },
+        },
+      ]}
+      {...getOverrideProps(overrides, "ReacticonsbiBiChevronLeft")}
+      {...rest}
+    ></Icon>
+  );
+}
