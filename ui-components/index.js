1d02dc319e9174f2ff932706bec0a344d2d65b21
timDeHof <this.is.tim.dehof@gmail.com>
2023-07-13 23:00:43 -0400

    feat: added myicon component

--- ui-components/index.js
+++ ui-components/index.js
@@ -6,23 +6,27 @@
 
 export { default as MarketingPricing } from "./MarketingPricing";
 export { default as PageCollection } from "./PageCollection";
+export { default as MyIcon } from "./MyIcon";
 export { default as ReacticonsbiBiMenu } from "./ReacticonsbiBiMenu";
 export { default as Footer } from "./Footer";
 export { default as Features2X2 } from "./Features2X2";
 export { default as Persuade } from "./Persuade";
 export { default as ReacticonsbiBiBookOpen } from "./ReacticonsbiBiBookOpen";
 export { default as Logo3 } from "./Logo3";
+export { default as Notebookitem } from "./Notebookitem";
 export { default as NotebookCollection } from "./NotebookCollection";
 export { default as Editor } from "./Editor";
 export { default as Nav } from "./Nav";
 export { default as Page } from "./Page";
 export { default as Hero } from "./Hero";
 export { default as ReacticonsbiBiMenuAltRight } from "./ReacticonsbiBiMenuAltRight";
+export { default as Sidebar } from "./Sidebar";
 export { default as PlanCard } from "./PlanCard";
 export { default as ReacticonsbiBiChevronRight } from "./ReacticonsbiBiChevronRight";
 export { default as ReacticonsbiBiMenuAltLeft } from "./ReacticonsbiBiMenuAltLeft";
 export { default as ReacticonsbiBiCheck } from "./ReacticonsbiBiCheck";
 export { default as ReacticonsbiBiChevronLeft } from "./ReacticonsbiBiChevronLeft";
+export { default as ReacticonsbiBiNote } from "./ReacticonsbiBiNote";
 export { default as Pages } from "./Pages";
 export { default as FeatureCard } from "./FeatureCard";
 export { default as FeaturesCollection } from "./FeaturesCollection";
